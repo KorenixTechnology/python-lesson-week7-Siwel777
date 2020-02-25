05ade4c67986b5359257562c71b2e5c6c53817fe
Siwel <zuytrew951753@gmail.com>
2020-02-25 14:03:49 +0800

    Suess Number first

--- hw_week7.py
+++ hw_week7.py
@@ -1,8 +1,19 @@
 import random
 
 def hw_week7(secret_number):
-    # Your code here
-    pass
+    i = 0   #count
+    while True :
+        i = i + 1   #counter
+        print('Take a guess.')
+        guess = int(input())
+        if guess == secret_number:
+            print('Good job! You guessed my number in ' + str(i) + ' guesses!')
+            break
+        elif guess > secret_number:
+            print('Your guess is too high.')
+        elif guess < secret_number:
+            print('Your guess is too low.')
+            
 
 
 if __name__ == '__main__':
